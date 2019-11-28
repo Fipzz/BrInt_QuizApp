c8cc81182d9dddc96ca141e8bc06f633e51fb280
Fipzz <Philiprumle@gmail.com>
2019-11-28 15:09:35 +0100

    setup for Main and login. Working, almost

--- app/src/main/java/com/example/brint_quizapp/MainActivity.java
+++ app/src/main/java/com/example/brint_quizapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.brint_quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -11,6 +12,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button student, lector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,67 +21,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        Button answer1 = (Button) findViewById(R.id.answer1);
-        answer1.setOnClickListener(this);
-        Button answer2 = (Button) findViewById(R.id.answer2);
-        answer2.setOnClickListener(this);
-        Button answer3 = (Button) findViewById(R.id.answer3);
-        answer3.setOnClickListener(this);
-        Button answer4 = (Button) findViewById(R.id.answer4);
-        answer4.setOnClickListener(this);
-        TextView questionView = (TextView) findViewById(R.id.questionView);
 
-        String[][] question = new String[2][7];
-        question[0][0] = "0";
-        question[0][1] = "Hvilken farve har faxe kondi flasken?";
-        question[0][2] = "Grøn";
-        question[0][3] = "Blå";
-        question[0][4] = "Rød";
-        question[0][5] = "Orange";
-        question[1][0] = "1";
-        question[1][1] = "Hvilken dag er den sidste dag på ugen?";
-        question[1][2] = "Søndag";
-        question[1][3] = "Tirsdag";
-        question[1][4] = "Torsdag";
-        question[1][5] = "Mandag";
+        student = (Button) findViewById(R.id.student);
+        student.setOnClickListener(this);
+        lector = (Button) findViewById(R.id.lector);
+        lector.setOnClickListener(this);
 
-        showNewQuestion(0, question, questionView, answer1, answer2, answer3, answer4);
+        TextView header = (TextView) findViewById(R.id.questionView);
+        header.setText("Quiz app (B5)");
 
     }
 
     @Override
     public void onClick(View v) {
-        Button answer1 = (Button) findViewById(R.id.answer1);
-        answer1.setOnClickListener(this);
-        Button answer2 = (Button) findViewById(R.id.answer2);
-        answer2.setOnClickListener(this);
-        Button answer3 = (Button) findViewById(R.id.answer3);
-        answer3.setOnClickListener(this);
-        Button answer4 = (Button) findViewById(R.id.answer4);
-        answer4.setOnClickListener(this);
-        TextView questionView = (TextView) findViewById(R.id.questionView);
-        String[][] question = new String[2][7];
-        question[0][0] = "0";
-        question[0][1] = "Hvilken farve har faxe kondi flasken?";
-        question[0][2] = "Grøn";
-        question[0][3] = "Blå";
-        question[0][4] = "Rød";
-        question[0][5] = "Orange";
-        question[1][0] = "1";
-        question[1][1] = "Hvilken dag er den sidste dag på ugen?";
-        question[1][2] = "Søndag";
-        question[1][3] = "Tirsdag";
-        question[1][4] = "Torsdag";
-        question[1][5] = "Mandag";
-        showNewQuestion(1, question, questionView, answer1, answer2, answer3, answer4);
-    }
 
-    public void showNewQuestion(int question_id, String[][] questions, TextView questionText, Button answer1Button, Button answer2Button, Button answer3Button, Button answer4Button){
-        questionText.setText(questions[question_id][1]);
-        answer1Button.setText(questions[question_id][2]);
-        answer2Button.setText(questions[question_id][3]);
-        answer3Button.setText(questions[question_id][4]);
-        answer4Button.setText(questions[question_id][5]);
+        if(R.id.student == v.getId()){
+
+            startActivity(new Intent(MainActivity.this, Login_student.class));
+
+        } else if(R.id.lector == v.getId()){
+
+            startActivity(new Intent(MainActivity.this, Login_lector.class));
+
+        }
 
     }
+
 }
