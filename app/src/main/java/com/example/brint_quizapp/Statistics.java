b2c3bde27e3d794cb08ab481f39b4d914e92dedb
MartinWassmann <s185033@student.dtu.dk>
2020-01-18 21:34:17 +0100

    Updated some of the relevant classes

--- app/src/main/java/com/example/brint_quizapp/Statistics.java
+++ app/src/main/java/com/example/brint_quizapp/Statistics.java
@@ -1,5 +1,6 @@
 package com.example.brint_quizapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -26,10 +27,47 @@ public class Statistics extends AppCompatActivity implements View.OnClickListene
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.statistics_activity_layout);
+
+        quizName = (TextView) findViewById(R.id.quizNavn);
+
+
+        //When one of the textviews has the Stat suffix, it's to the right of the other one and is meant to track whichever the correspending statistic is
+        amountQuestions = (TextView) findViewById(R.id.amountQuestions);
+        amountCompletionStat = (TextView) findViewById(R.id.amountCompletionStat);
+
+        amountCompletion = (TextView) findViewById(R.id.amountCompletion);
+        amountCompletionStat = (TextView) findViewById(R.id.amountCompletionStat);
+
+        avgCompletionTime = (TextView) findViewById(R.id.avgCompletionTime);
+        avgCompletionTimeStat = (TextView) findViewById(R.id.avgCompletionTimeStat);
+
+        avgCompletionTime = (TextView) findViewById(R.id.avgCompletionTime);
+        avgCompletionTimeStat = (TextView) findViewById(R.id.avgCompletionTimeStat);
+
+        avgCorrect = (TextView) findViewById(R.id.avgCorrect);
+        avgCorrectStat = (TextView) findViewById(R.id.avgCorrectStat);
+
+        avgCorrectPercent = (TextView) findViewById(R.id.avgCorrectPercent);
+        avgCorrectPercentStat = (TextView) findViewById(R.id.avgCorrectPercentStat);
+
+
+        graphs = (Button) findViewById(R.id.graphs);
+
+        questionStats = (Button) findViewById(R.id.questionStats);
+
+        backButton = (Button) findViewById(R.id.backButton);
+
     }
 
     @Override
     public void onClick(View v) {
-
+        //TODO Update intents and create their corresponding classes
+        if (R.id.graphs == v.getId()) {
+            //startActivity(new Intent(Statistics.this, Graph_Statistics.class));
+        } else if (R.id.questionStats == v.getId()) {
+            //startActivity(new Intent(Statistics.this, QuestionStats_Statistics.class));
+        } else if (R.id.backButton == v.getId()) {
+            startActivity(new Intent(Statistics.this, Edit_Quiz.class));
+        }
     }
 }
