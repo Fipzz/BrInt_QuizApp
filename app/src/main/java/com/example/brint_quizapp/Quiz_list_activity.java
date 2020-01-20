ecc56d151ae97de6c34a3b941584194bae7c2871
Fipzz <Philiprumle@gmail.com>
2020-01-20 16:06:44 +0100

    Removed old imports from quiz list

--- app/src/main/java/com/example/brint_quizapp/Quiz_list_activity.java
+++ app/src/main/java/com/example/brint_quizapp/Quiz_list_activity.java
@@ -18,8 +18,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.brint_quizapp.dal.dto.QuizDTO;
 import com.google.firebase.internal.InternalTokenProvider;
