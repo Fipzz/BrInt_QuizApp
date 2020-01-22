bc8444fcb05f08409e0d5c9ca13b8a7ad5655f67
Razmuz99 <razmuz99@gmail.com>
2020-01-22 20:23:28 +0100

    bug fix.

--- app/src/main/java/com/example/brint_quizapp/Edit_Quiz.java
+++ app/src/main/java/com/example/brint_quizapp/Edit_Quiz.java
@@ -25,9 +25,6 @@ import com.example.brint_quizapp.dal.dto.QuizDTO;
 import java.sql.Connection;
 import java.util.ArrayList;
 
-import io.sentry.Sentry;
-import io.sentry.android.AndroidSentryClientFactory;
-
 public class Edit_Quiz extends AppCompatActivity implements View.OnClickListener{
 
     //TODO add a button to remove a question
